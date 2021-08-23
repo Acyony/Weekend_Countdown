e36e1a9ff85c18ef486191347eae3c7e458aed22
Acyony <42450397+Acyony@users.noreply.github.com>
2021-08-23 17:34:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
+//Formate the data => Intl.DateTimeFormat
+
+function formateDate(dateObject) {
+  let formatter = new Intl.DateTimeFormat("en", { weekday: "long" });
+  return formatter.format(dateObject);
+}
+
+// To print the message on the browser
+
+function doClick() {
+  let now = new Date();
+  let myDateFormatted = formateDate(now);
+  let dayOfWeek = now.getDay();
+  let input = document.getElementById("username");
+
+
+  let emptyInputMessage = "Please enter your name below!";
+  let message = `Hello ${input.value}, today is ${myDateFormatted}. Only ${
+    5 - dayOfWeek
+  } left until the weekend!`;
+
+  if (input.value === " ") {
+    document.getElementById("h3_id").innerHTML = emptyInputMessage;
+  } else {
+    document.getElementById("h3_id").innerHTML = message;
+  }
+}
+
+// to clear the input text when refresh the page
+
+function init() {
+  document.getElementById("username").value = " ";
+}
+window.onload = init;
 
