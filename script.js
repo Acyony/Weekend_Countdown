7075ead990f193cf991d45ef50aa32e0262538ff
Alcione <alcionefribeiro@gmail.com>
2022-06-22 20:55:55 +0200

    updating the function to to show also the weekend days

--- script.js
+++ script.js
@@ -1,35 +1,41 @@
 //Formate the data => Intl.DateTimeFormat
-
 function formateDate(dateObject) {
-  let formatter = new Intl.DateTimeFormat("en", { weekday: "long" });
-  return formatter.format(dateObject);
+    let formatter = new Intl.DateTimeFormat("en", {weekday: "long"});
+    return formatter.format(dateObject);
 }
 
 // To print the message on the browser
-
 function doClick() {
-  let now = new Date();
-  let myDateFormatted = formateDate(now);
-  let dayOfWeek = now.getDay();
-  let input = document.getElementById("username");
+    let now = new Date();
+    let myDateFormatted = formateDate(now);
+    let dayOfWeek = now.getDay();
+    let input = document.getElementById("username");
 
+    let emptyInputMessage = "Please enter your name below!";
+    let message;
 
-  let emptyInputMessage = "Please enter your name below!";
-  let message = `Hello ${input.value}, today is ${myDateFormatted}. Only ${
-    5 - dayOfWeek
-  } left until the weekend!`;
+    if (dayOfWeek === 6 || dayOfWeek === 7) {
+        message = `Hello ${input.value}, today is ${myDateFormatted}. Enjoy the weekend!`;
+    } else {
+        message = `Hello ${input.value}, today is ${myDateFormatted}. Only ${
+            5 - dayOfWeek
+        } left until the weekend!`
+    }
 
-  if (input.value.trim() === "") {
-    document.getElementById("h3_id").innerHTML = emptyInputMessage;
-  } else {
-    document.getElementById("h3_id").innerHTML = message;
-  }
+    if (input.value.trim() === "") {
+        document.getElementById("h3_id").innerHTML = emptyInputMessage;
+    } else if (input.value.trim() !== "") {
+        document.getElementById("h3_id").innerHTML = message;
+    } else {
+        document.getElementById("h3_id").innerHTML = gratulations;
+
+    }
 }
 
 // to clear the input text when refresh the page
-
 function init() {
-  document.getElementById("username").value = "";
+    document.getElementById("username").value = "";
 }
+
 window.onload = init;
 
